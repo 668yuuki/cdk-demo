11879eec35b3148a647206ac78e1495226b6950d
668yuuki <yuuuki668@gmail.com>
2023-05-24 04:04:09 +0900

    codePipeline対応

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkDemoEcrStack } from "../lib/cdk-demo-ecr-stack";
+import { CdkDemoCodePipelineStack } from "../lib/cdk-demo-codePipeline-stack";
 
 const app = new cdk.App();
-new CdkDemoStack(app, 'CdkDemoStack', {
-  env: { account: '116707747645', region: 'ap-northeast-1' },
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = { account: "116707747645", region: "ap-northeast-1" }
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new CdkDemoEcrStack(app, "CdkDemoEcrStack", {
+  env,
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '116707747645', region: 'ap-northeast-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CdkDemoCodePipelineStack(app, "CdkDemoCodePipelineStack", {
+  env,
 });
---
+++ lib/cdk-demo-codePipeline-stack.ts
@@ -0,0 +1,105 @@
+import {
+  aws_ecr,
+  aws_ecs,
+  aws_codepipeline,
+  aws_codepipeline_actions,
+  aws_codebuild,
+  Stack,
+  StackProps,
+  aws_ecs_patterns,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as yaml from 'yaml';
+
+export class CdkDemoCodePipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const demoPipeline = new aws_codepipeline.Pipeline(this, "DemoPipeline");
+    const sourceOutput = new aws_codepipeline.Artifact();
+    const buildOutput = new aws_codepipeline.Artifact();
+    const ecrRepository = aws_ecr.Repository.fromRepositoryName(this, "repo1", "demo/repository_2023/0509_11");
+
+    // Source Stage
+    const sourceAction = new aws_codepipeline_actions.EcrSourceAction({
+      actionName: "ECR_Source",
+      repository: ecrRepository,
+      output: sourceOutput,
+    });
+    demoPipeline.addStage({
+      stageName: "Source",
+      actions: [sourceAction],
+    });
+
+    const taskDefinition = new aws_ecs.FargateTaskDefinition(this, 'TaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+    taskDefinition.addContainer("WebContainer", {
+      image: aws_ecs.ContainerImage.fromEcrRepository(ecrRepository),
+      portMappings: [{ containerPort: 8080 }],
+    });
+    
+    const demoFargateService = new aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, "DemoFargateService", {
+      taskDefinition,      
+      publicLoadBalancer: true,
+    });
+    // const cluster = new aws_ecs.Cluster(this, 'Cluster');
+    // const taskDefinition = new aws_ecs.FargateTaskDefinition(this, 'TaskDef', {
+    //   memoryLimitMiB: 512,
+    //   cpu: 256,
+    // });
+    // taskDefinition.addContainer("WebContainer", {
+    //   image: aws_ecs.ContainerImage.fromEcrRepository(ecrRepository),
+    //   portMappings: [{ containerPort: 8080 }],
+      
+    // });
+    // const demoFargateService = new aws_ecs.FargateService(this, 'SerDemoFargateServicevice', {
+    //   cluster,
+    //   taskDefinition
+    // });
+    const fromYaml = yaml.parse(`
+    version: 0.2
+
+    phases:
+      build:
+        commands:
+          - echo Build started
+          - printf '[{"name":"WebContainer","imageUri":"116707747645.dkr.ecr.ap-northeast-1.amazonaws.com/demo/repository_2023/0509_11:latest"}]' > imagedefinitions.json
+    artifacts:
+        files: imagedefinitions.json
+  `);
+
+    const project = new aws_codebuild.PipelineProject(this, "DemoProject", {
+      buildSpec: aws_codebuild.BuildSpec.fromObjectToYaml(fromYaml)
+    });
+
+    const buildAction = new aws_codepipeline_actions.CodeBuildAction({
+      actionName: 'CodeBuild',
+      project: project,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+    
+    demoPipeline.addStage({
+      stageName: 'Build',
+      actions: [buildAction],
+    });
+
+    // Deploy Stage
+    const deployAction = new aws_codepipeline_actions.EcsDeployAction({
+      actionName: "Fargate_Deploy",
+      // service: new aws_ecs.FargateService(this, 'SerDemoFargateServicevice', {
+      //   cluster,
+      //   taskDefinition
+      // }),
+      service: demoFargateService.service,
+      input: buildOutput,
+    });
+
+    demoPipeline.addStage({
+      stageName: "Deploy",
+      actions: [deployAction],
+    });
+ }
+}
